5f3e469d03441dfdb55ff4c036e972b3a41864e2
Daniel <danielps99@gmail.com>
2022-05-01 22:03:57 -0300

    feat: getWebDriverWait e waitUntilBeClickable no DriverService

--- src/main/java/br/com/bdws/facebookRobot/service/DriverService.java
+++ src/main/java/br/com/bdws/facebookRobot/service/DriverService.java
@@ -2,9 +2,13 @@ package br.com.bdws.facebookRobot.service;
 
 import br.com.bdws.facebookRobot.dto.Comando;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,5 +44,13 @@ public class DriverService {
             driver = new ChromeDriver(options);
         }
         return driver;
+    }
+
+    public WebDriverWait getWebDriverWait() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(30));
+    }
+
+    public void waitUntilBeClickable(WebElement webElement) {
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(webElement));
     }
 }
